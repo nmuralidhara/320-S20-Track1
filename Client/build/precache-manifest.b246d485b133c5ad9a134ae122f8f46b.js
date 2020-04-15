516dd16f9a6734c236eece34160602349a69d05f
Sean Powell <powellsl99@gmail.com>
2020-04-15 12:19:41 -0400

    change navbar and add match demo

---
+++ Client/build/precache-manifest.b246d485b133c5ad9a134ae122f8f46b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75171bb9a6aacb33c98af7acf9b41a68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d27092a7217d7fc1fbec",
+    "url": "/static/css/2.e0b663ed.chunk.css"
+  },
+  {
+    "revision": "ae8e1a3d81285da05c41",
+    "url": "/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "d27092a7217d7fc1fbec",
+    "url": "/static/js/2.7568e12e.chunk.js"
+  },
+  {
+    "revision": "42a8dc14db60071615398b3e804bbd8c",
+    "url": "/static/js/2.7568e12e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae8e1a3d81285da05c41",
+    "url": "/static/js/main.32f3ce04.chunk.js"
+  },
+  {
+    "revision": "7745faa89d5be321c93c",
+    "url": "/static/js/runtime-main.f40fbc3b.js"
+  }
+]);
